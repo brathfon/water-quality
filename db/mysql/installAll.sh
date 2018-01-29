f2973eae78b551adc7360ad3d50107a3fb0c47e8
brathfon <billrathfon@gmail.com>
2018-01-28 20:28:23 -1000

    Updated data from Google Sheets to sessions 28-33 and 1-6

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -3,4 +3,5 @@ mysql -v -u wq_dba -pntus < generated-schema-2.sql
 mysql -v -u wq_dba -pntus < insertData.sql
 mysql -v -u wq_dba -pntus < views.sql
 mysql -v -u wq_dba -pntus < procedures.sql
-mysql -v -u wq_dba -pntus < ../xfer/insert-data-sessions-28-30-1-2.sql
+#mysql -v -u wq_dba -pntus < ../xfer/insert-data-sessions-28-30-1-2.sql
+mysql -v -u wq_dba -pntus < ../xfer/insert-data-sessions-28-33-1-6.sql
