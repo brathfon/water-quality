47a163177dff0ab249374719f6945fc80d5494e2
brathfon <billrathfon@gmail.com>
2018-05-15 10:36:37 -1000

    changes do to add in new data from sessions 37 and 38 west and 11, 12, and 13 south

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -12,6 +12,6 @@ mysql -v -u $user -p$pw $db < auth-procedures.sql
 mysql -v -u $user -p$pw $db < reporting-procedures.sql
 mysql -v -u $user -p$pw $db < session-procedures.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-webexport-legacy-data.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-36-1-10.post-worker-changes.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180402.sessions-thru-33w-6s.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-38-1-13.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180515.0.sessions-thru-33w-6s.sql
 exit
