fb3898a7d6f6d908f29e03415ccf6b9c09b2ba73
brathfon <billrathfon@gmail.com>
2018-05-15 12:34:20 -1000

    added comments to parsing and insertion of gdrive spread sheet data

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -5,13 +5,14 @@ user=wq_dba
 pw=ntus
 
 
-mysql -v -u $user -p$pw < generated-schema-4.worker-changes.sql
+#mysql -v -u $user -p$pw < generated-schema-4.worker-changes.sql
+mysql -v -u $user -p$pw < generated-schema-4.1.increase-comment-size.sql
 mysql -v -u $user -p$pw < insertData.sql
 mysql -v -u $user -p$pw $db < views.sql
 mysql -v -u $user -p$pw $db < auth-procedures.sql
 mysql -v -u $user -p$pw $db < reporting-procedures.sql
 mysql -v -u $user -p$pw $db < session-procedures.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-webexport-legacy-data.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-38-1-13.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-38-1-13.1.with-comments.sql
 mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180515.0.sessions-thru-33w-6s.sql
 exit
