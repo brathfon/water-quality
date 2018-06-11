4e2be38095e991e3bb7280c34166309dda3718bb
brathfon <billrathfon@gmail.com>
2018-06-11 13:53:05 -1000

    found a double dash in a date in spread sheet. MySQL insert was able to handle it, but added a check in reader to report it

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -14,7 +14,6 @@ mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw < insertData.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-webexport-legacy-data.1.first-sample-day-change.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-39-1-15.1.no-workers.sql
-#mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180603.0.sessions-thr-33w-6s.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-39-1-15.2.fixed-double-dash-dates.sql
 mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180610.0.sessions-thru-36w-11s.sql
 exit
