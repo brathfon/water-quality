ee0ed600e3bccc20ffec838f689bf09a39a42997
brathfon <billrathfon@gmail.com>
2018-11-12 15:52:29 -1000

    added in insitu data for sessions 46 47 west and 21 22 south

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -14,6 +14,7 @@ mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-webexport-legacy-data.1.first-sample-day-change.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-45-1-20.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180930.0.sessions-thru-39w-15s.insitu-w45-s20.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-47-1-22.0.sql
+#mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20180930.0.sessions-thru-39w-15s.insitu-w45-s20.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20181111.0.sessions-thru-39w-15s.insitu-w47-s22.sql
 exit
--- db/test/reports/diff-last-two-cksums.sh
+++ db/test/reports/diff-last-two-cksums.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 
 echo "diffs for commments-report"
-diff -w `ls comments-report.2018-0* | tail -2`
+diff -w `ls comments-report.2018-* | tail -2`
 
 echo ""
 echo "diffs for insitu-report"
-diff -w  `ls insitu-report.2018-0* | tail -2`
+diff -w  `ls insitu-report.2018-* | tail -2`
 
 echo ""
 echo "diffs for nutrient-report"
-diff -w  `ls nutrient-report.2018-0* | tail -2`
+diff -w  `ls nutrient-report.2018-* | tail -2`
 
 echo ""
 echo "diffs for sample-report"
-diff -w `ls sample-report.2018-0* | tail -2`
+diff -w `ls sample-report.2018-* | tail -2`
