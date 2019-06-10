24338adbf80472930fe5c36f0b6256335a21cc92
Bill Rathfon <billrathfon@gmail.com>
2019-06-10 17:08:08 -0400

    adding nutrient data from sessions 53 west and 28-29 south along with new 2019 1st quarter web exports that do not include insitu only samples

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -16,6 +16,6 @@ mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-webexport-legacy-data.3.add-sampling-order.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-28-56-1-32.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20190604.0.nutrient-52w-27s.insitu-56s-32s.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20190610.0.nutrient-523-29s.insitu-56s-32s.sql
 
 exit
