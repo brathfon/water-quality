b99637b1a676041316ecae907b590bd12ed82f80
Bill Rathfon <billrathfon@gmail.com>
2019-10-08 10:58:41 -0400

    created 3rd quarter 2019 web exports.  Changed separator of comments from comma to semi-colon so CSV files are easier to injest by users

--- db/reports/create-webexport.js
+++ db/reports/create-webexport.js
@@ -154,7 +154,7 @@ var descriptionObjToString = function (obj) {
   for (i = 0; i < keys.length; ++i) {
      str += keys[i];
    if (i !== (keys.length - 1)) {
-     str +=  ", ";
+     str +=  "; ";
    }
   }
   return str;
