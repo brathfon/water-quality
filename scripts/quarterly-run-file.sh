caf3e10e4b5036e3b93769994c95976e397267de
Bill Rathfon <billrathfon@gmail.com>
2020-01-12 19:45:12 -0500

    Adding insitu data for sessions 66 west and 42 south. Altered message for when dates and sampleIDs are not consistent.

---
+++ scripts/quarterly-run-file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wq_files=/Users/bill/development/water-quality/water-quality-data
+
+output_dir=../reports/web-export-quarterly-reports
+
+./create-web-export-file-based.js  \
+   --odir $output_dir \
+   --bname 4th-quarter  \
+   --gsdir  "$wq_files/gdrive-downloads/data-sessions-28-66w-1-42s" \
+   --lfile "$wq_files/legacy-data/west-maui-legacy-data-sessions-1-27.tsv" \
+   --sfile ./sites.txt  \
+   --ndir  $wq_files/soest/csv-files \
+
+
+#   --inns   # this is for removing data without nutrients option
+
