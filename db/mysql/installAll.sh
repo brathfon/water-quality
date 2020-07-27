369bf95894df2f3f7752bf975dcee5b58c63b3ed
Bill Rathfon <billrathfon@gmail.com>
2020-07-27 18:15:15 -0400

    added insitu 71-73w and 46-48s and nutrient data 70-71w and 46s

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -14,7 +14,7 @@ mysql -v -u $user -p$pw $db < site-procedures.sql
 mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-70w-1-45s.1.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20200527.0.nutrient-69w-45s.insitu-70w-45s.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-73w-1-48s.0.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20200726.0.nutrient-71w-46s.insitu-73w-48s.sql
 
 exit
