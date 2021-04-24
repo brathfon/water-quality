6f3f13393d78c896493ad703fa68c027e3c0d3ac
Bill Rathfon <billrathfon@gmail.com>
2021-04-24 17:01:22 -0400

    Added nutrient data 82-84 West and 57-60 South

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -15,6 +15,6 @@ mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-85w-1-61s.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20210416.0.nutrient-81w-56s.insitu-85w-61s.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20210418.0.nutrient-84w-60s.insitu-85w-61s.sql
 
 exit
