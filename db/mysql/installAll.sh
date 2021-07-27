dfa2aee3ade7b032241d5ac41b89dcf1380bcb59
Bill Rathfon <billrathfon@gmail.com>
2021-07-27 14:19:54 -0400

    Adding final data for 2021 2nd quarter release.  Includes nutrient data thru sessions 89 West Maui and 64 South Maui and insitu data up to 90 West Maui and 65 South Maui.

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -14,7 +14,7 @@ mysql -v -u $user -p$pw $db < site-procedures.sql
 mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-89w-1-64s.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20210630.0.nutrient-86w-61s.insitu-89w-64s.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-90w-1-65s.0.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20210720.0.nutrient-89w-64s.insitu-90w-65s.sql
 
 exit
