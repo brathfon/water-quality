00b10616f9d0e597bb9d952574989d66c81e3701
Bill Rathfon <billrathfon@gmail.com>
2022-04-17 20:12:48 -0400

    Adding in nutrient data for sessions 99-100 West Maui and 75-76 South Maui

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -15,6 +15,6 @@ mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
 mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-100w-1-76s.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20220322.0.nutrient-98w-74s.insitu-100w-76s.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20220414.0.nutrient-100w-76s.insitu-100w-76s.sql
 
 exit
