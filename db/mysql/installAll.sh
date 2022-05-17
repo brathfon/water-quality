710ece2a623ccef08735b4fd686ec4372bd8ac99
Bill Rathfon <billrathfon@gmail.com>
2022-05-17 10:37:01 -0400

    Added in insitu data for sessions 101-102 West Maui and 77-78 South Maui in preparation for 1st quarter release for 2022.

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -14,7 +14,7 @@ mysql -v -u $user -p$pw $db < site-procedures.sql
 mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-100w-1-76s.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20220414.0.nutrient-100w-76s.insitu-100w-76s.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-102w-1-78s.0.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20220418.0.nutrient-100w-76s.insitu-102w-78s.sql
 
 exit
