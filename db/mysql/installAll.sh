c5badea5d635f8826df55370896d61a3023ec673
Bill Rathfon <billrathfon@gmail.com>
2023-04-17 14:07:19 -0400

    added in insitu for sessions 116-117 West Maui and sessions 90-93 South Maui

--- db/mysql/installAll.sh
+++ db/mysql/installAll.sh
@@ -14,6 +14,6 @@ mysql -v -u $user -p$pw $db < site-procedures.sql
 mysql -v -u $user -p$pw $db < lookup-procedures.sql
 mysql -v -u $user -p$pw $db < utility-procedures.sql
 mysql -v -u $user -p$pw $db < insertData.sql
-mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-115w-1-90s.0.sql
-mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20230203.0.nutrient-114w-90s.insitu-115w-90s.sql
+mysql -v -u $user -p$pw $db < ../xfer/insert-data-sessions-1-117w-1-93s.0.sql
+mysql -v -u $user -p$pw $db < ../xfer/update-nutrient-data.20230402.0.nutrient-114w-90s.insitu-117w-93s.sql
 exit
